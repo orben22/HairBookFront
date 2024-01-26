605beaf39d88324487d318985a1eaf7916dec9e5
Chen Ben Ami <chen24201@gmail.com>
2024-01-26 13:56:29 +0200

    ReviewsComponent.kt editable

--- app/src/main/java/com/example/hairbookfront/ui/common/ReviewsComponent.kt
+++ app/src/main/java/com/example/hairbookfront/ui/common/ReviewsComponent.kt
@@ -7,43 +7,54 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.hairbookfront.domain.entities.BarberShop
+import androidx.compose.ui.unit.dp
 import com.example.hairbookfront.domain.entities.Review
 import com.example.hairbookfront.theme.HairBookFrontTheme
 import com.example.hairbookfront.ui.Dimens
 
 @Composable
 fun ReviewsList(
-    reviews: List<Review>?, editable: Boolean = false,
+    reviews: List<Review>?,
+    editable: Boolean = false,
     onClickFunctions: List<() -> Unit> = listOf(
         { },
-        { }
-    )
+        { },
+    ),
+    isEditing: Boolean = false,
+    onReviewChange: (String) -> Unit = {},
+    onRatingChange: (String) -> Unit = {},
+    mode: String = "Customer",
+    isError: Boolean = false,
 ) {
     reviews?.let {
-        LazyColumn {
-            items(reviews) { review ->
+        Column {
+            for (review in reviews) {
                 ReviewItem(
                     review = review,
                     editable = editable,
-                    onClickFunctions = onClickFunctions
+                    onClickFunctions = onClickFunctions,
+                    isEditing = isEditing,
+                    onReviewChange = onReviewChange,
+                    onRatingChange = onRatingChange,
+                    mode = mode
                 )
             }
         }
@@ -52,11 +63,17 @@ fun ReviewsList(
 
 @Composable
 fun ReviewItem(
-    review: Review, editable: Boolean,
+    review: Review,
+    editable: Boolean,
     onClickFunctions: List<() -> Unit> = listOf(
         { },
-        { }
+        { },
     ),
+    isEditing: Boolean = false,
+    onReviewChange: (String) -> Unit = {},
+    onRatingChange: (String) -> Unit = {},
+    mode: String = "Customer",
+    isError: Boolean = false
 ) {
     OutlinedCard(
         colors = CardDefaults.cardColors(
@@ -73,32 +90,60 @@ fun ReviewItem(
                 fontSize = Dimens.fontMedium,
             )
             Spacer(modifier = Modifier.height(Dimens.smallPadding1))
-            Text(
-                text = review.review,
-                fontSize = Dimens.fontMedium,
-            )
-            Spacer(modifier = Modifier.height(Dimens.smallPadding1))
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = "Time: " + review.timestamp,
-                    fontSize = Dimens.fontSmall,
-                    fontWeight = FontWeight.Bold,
+            if (isEditing) {
+                TextField(
+                    value = review.review,
+                    onValueChange = onReviewChange,
+                    modifier = Modifier.padding(Dimens.smallPadding1)
                 )
-                RatingComponent(rating = review.rating)
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    TextField(
+                        value = review.rating.toString(),
+                        onValueChange = onRatingChange,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        modifier = Modifier
+                            .width(80.dp)
+                            .padding(Dimens.smallPadding1),
+                        isError = isError
+                    )
+                }
+            } else {
+                Text(
+                    text = review.review,
+                    fontSize = Dimens.fontMedium,
+                )
+                Spacer(modifier = Modifier.height(Dimens.smallPadding1))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "Time: " + review.timestamp,
+                        fontSize = Dimens.fontSmall,
+                        fontWeight = FontWeight.Bold,
+                    )
+                    RatingComponent(rating = review.rating)
+                }
             }
             Spacer(modifier = Modifier.height(Dimens.smallPadding1))
-            if (editable) {
+            if (editable && mode == "Customer") {
                 Row(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(Dimens.smallPadding3),
                     horizontalArrangement = Arrangement.SpaceBetween
                 ) {
-                    IconButton(onClick = { onClickFunctions[0]() }) {
-                        Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+                    if (isEditing) {
+                        IconButton(onClick = { onClickFunctions[0]() }) {
+                            Icon(imageVector = Icons.Default.Check, contentDescription = "Check")
+                        }
+                    } else {
+                        IconButton(onClick = { onClickFunctions[0]() }) {
+                            Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit")
+                        }
                     }
                     IconButton(onClick = { onClickFunctions[1]() }) {
                         Icon(imageVector = Icons.Default.Delete, contentDescription = "Remove")
@@ -124,7 +169,9 @@ fun ReviewItemPreview() {
                 userId = "userId",
                 barbershopId = "barbershopId"
             ),
-            editable = true
+            editable = true,
+            isEditing = true,
+            mode = "Customer"
         )
     }
 }
